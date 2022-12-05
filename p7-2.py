c66ce46886e1aa3c91ec2549b3d655846d04c6c4
tarni-s-p <103916144+tarni-s-p@users.noreply.github.com>
2022-12-05 11:32:27 +0530

    Add files via upload

--- p7-2.py
+++ p7-2.py
@@ -4,9 +4,14 @@ s2={10,20,60,80,70,90}
 print(s1)
 print(s2)
 
-print("remove elements")
-s1.discard(s2)
+print("add elements")
+s1.add(50)
 print(s1)
+
+#print("remove elements")
+#s1.remove(s2)
+#print(s1)
+
 print("elements are union")
 print(s1|s2)
 
@@ -19,4 +24,6 @@ print(s2-s1)
 
 print("elements are symmetric difference")
 print(s1^s2)
-print(s2^s1)
+
+print("elements check subset of another set")
+print(s1<s2)
